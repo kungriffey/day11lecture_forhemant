f5da889c7931395e5d8a7c62ecb3040e82559ede
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-20 10:40:58 -0400

    created 3 properties - name, type, cook time

---
+++ Recipe.h
@@ -0,0 +1,18 @@
+//
+//  Recipe.h
+//  ObjectOrientedLecture
+//
+//  Created by Kunwardeep Gill on 2015-04-20.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Recipe : NSObject
+
+
+@property (strong, nonatomic) NSString *recipeName;
+@property (strong, nonatomic) NSString *recipeType;
+@property (strong, nonatomic) NSNumber *cookTime;
+
+@end
---
+++ Recipe.m
@@ -0,0 +1,13 @@
+//
+//  Recipe.m
+//  ObjectOrientedLecture
+//
+//  Created by Kunwardeep Gill on 2015-04-20.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import "Recipe.h"
+
+@implementation Recipe
+
+@end
